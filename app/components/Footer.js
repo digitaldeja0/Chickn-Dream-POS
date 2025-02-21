343f2291259681efb79015270e1d610435d1d9c1
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2025-02-21 05:50:21 -0500

    Updated central POS section in home page.

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <>
     <footer className="footer footer-center bg-base-300 text-base-content p-4">
   <aside>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by Chickn Dream Ltd</p>
   </aside>
 </footer>
 
--- app/page.js
+++ app/page.js
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-      Testing 123
-        
-      </main>
+    <div className="flex flex-col items-center justify-items-center min-h-screen  p-4 pb-8 gap-4 sm:p-4 font-[family-name:var(--font-geist-sans)]">
+      <div className="flex flex-col md:flex-row w-full flex-grow  p-4">
+        <section className="w-full md:w-1/2 border-2 border-red-100  flex-grow flex flex-col items-start justify-start p-4 ">
+          MENU
+        </section>
+        <section className="w-full md:w-1/2 border-2 border-red-100  flex-grow flex flec-col items-start justify-start p-4">
+          RECIPET
+        </section>
+      </div>
     </div>
   );
 }
